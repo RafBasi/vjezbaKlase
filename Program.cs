9fefdccd4e13b07daf49769cd678ea9327f6fc17
RafBasi <78168872+RafBasi@users.noreply.github.com>
2023-11-29 09:33:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaKlase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            zrakoplov pop34 = new zrakoplov("B-201",1456,8234);
+            Console.WriteLine(pop34.ToString());
+
+            zrakoplov x350 = new zrakoplov("Airbus",123, 8234);
+            Console.WriteLine(x350.ToString());
+
+            zrakoplov pc32 = new zrakoplov("Giga101", 14326, 8234);
+            Console.WriteLine(pc32.ToString());
+            Console.ReadKey();
+        }
+
+    }
+
---
+++ zrakoplov.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaKlase
+{
+    internal class zrakoplov
+    {
+        string naziv;
+        double snagaMotora;
+        int dosegLeta;
+
+        public zrakoplov(string naziv, double snagaMotora, int dosegLeta)
+        {
+            this.naziv = naziv;
+            this.snagaMotora = snagaMotora;
+            this.dosegLeta = dosegLeta;
+        }
+
+        public string Naziv { get => naziv; set => naziv = value; }
+        public double SnagaMotora { get => snagaMotora; set => snagaMotora = value; }
+        public int DosegLeta { get => dosegLeta; set => dosegLeta = value; }
+
+        public override string ToString()
+        {
+            string ispis = "Naziv aviona: " + this.Naziv + "\n"
+                + "Snaga motora: " + this.SnagaMotora + "kW\n"
+                + "Doseg leta: " + this.DosegLeta + "km\n";
+            return ispis;
+        }
+    }
+}
